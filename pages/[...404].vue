12d851a5aef93fb1eaaf61bcd95d1894a2c1981f
pi-dal <hi@pi-dal.com>
2025-07-17 10:43:51 +0800

    feat: restore 404 page
    Add back the missing 404 page that was deleted in a previous cleanup.

---
+++ pages/[...404].vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto px-4 py-20 text-center">
+    <h1 class="text-6xl font-bold mb-4">
+      404
+    </h1>
+    <p class="text-xl text-gray-600 dark:text-gray-400">
+      Nice to meet you tho!
+    </p>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>

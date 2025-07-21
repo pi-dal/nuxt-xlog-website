df6e7f5cd6e39953456f239efe53cf8fcdea1e99
pi-dal <hi@pi-dal.com>
2025-07-21 10:04:00 +0800

    fix: properly hide RSS menu on mobile devices
    - Wrap RSSMenu component in div with lt-md:hidden class
    - Ensures RSS menu is properly hidden on mobile devices (smaller than md breakpoint)
    - Improves mobile navigation by reducing interface clutter

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -48,7 +48,9 @@ const { y: scroll } = useWindowScroll()
         <a href="https://photography.pi-dal.com" target="_blank" title="Photos">
           <div i-ri-camera-3-line />
         </a>
-        <RSSMenu class="lt-md:hidden" />
+        <div class="lt-md:hidden">
+          <RSSMenu />
+        </div>
         <ToggleTheme />
       </div>
     </nav>
